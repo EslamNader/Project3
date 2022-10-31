fd0bb2ab4d138fe4f970fb7e4025b79f7bf3afb8
eslamnader <eslamnader@outlook.com>
2022-10-31 21:26:14 +0200

    Add files

---
+++ END-monolith-to-microservices-project-main/set_env.sh
@@ -0,0 +1,12 @@
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=SpringSpring1
+export POSTGRES_HOST=postgres.ceuyvkh6r57k.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=project3-5435518497-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
+export DOCKER_USERNAME=marwaend22
+export DOCKER_PASSWORD=P@ssw0rd2022
---
+++ END-monolith-to-microservices-project-main/udagram-api-feed/src/config/config.ts
@@ -0,0 +1,14 @@
+export const config = {
+  'username':'postgres',
+  'password':'SpringSpring1',
+  'database':'postgres',
+  'host':'postgres.ceuyvkh6r57k.us-east-1.rds.amazonaws.com',
+  'dialect': 'postgres',
+  'aws_region':'us-east-1',
+  'aws_profile':'default',
+  'aws_media_bucket':'project3-5435518497-dev',
+  'url': 'http://localhost:8100',
+  'jwt': {
+    'secret':'testing',
+  },
+};
